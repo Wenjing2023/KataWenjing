69d955a292e0f84add75b020c6c93bf2c49e1a24
GPSPic <picard.guillaume47@gmail.com>
2023-08-29 14:04:00 +0100

    added transaction creation function

--- kata_wen_gui/BankAccountKata/src/main/java/org/example/BankAccount.java
+++ kata_wen_gui/BankAccountKata/src/main/java/org/example/BankAccount.java
@@ -1,8 +1,9 @@
 package org.example;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class BankAccount {
+public class BankAccount implements BankAccountController {
     private String ownerName;
     private int currentBalance;
 
@@ -38,4 +39,30 @@ public class BankAccount {
     public void setTransactions(ArrayList<Transaction> transactions) {
         this.transactions = transactions;
     }
+
+    public void addTransaction(Transaction transaction) {
+        this.transactions.add(transaction);
+    }
+
+    public boolean isDeposit(Transaction transaction) {
+        int amount = transaction.getAmount();
+        return amount >= 0;
+    }
+
+    public Transaction createTransaction(int amount) {
+        LocalDate date = LocalDate.now();
+        return new Transaction(date, amount);
+    }
+
+    public void deposit(int amount) {
+        this.addTransaction(this.createTransaction(amount));
+    };
+
+    public void withdraw(int amount) {
+        this.addTransaction((this.createTransaction(amount)));
+    };
+
+    public void printStatement() {
+
+    };
 }
