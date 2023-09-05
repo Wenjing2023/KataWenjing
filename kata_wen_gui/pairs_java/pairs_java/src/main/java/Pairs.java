577ed16af7add985955b6a6dd21a2e29c5adb030
GPSPic <picard.guillaume47@gmail.com>
2023-09-05 15:16:26 +0100

    completed kata

--- kata_wen_gui/pairs_java/pairs_java/src/main/java/Pairs.java
+++ kata_wen_gui/pairs_java/pairs_java/src/main/java/Pairs.java
@@ -7,29 +7,15 @@ public class Pairs {
     }
     public int findPairs(ArrayList<Integer> arr, int n){
         int counter = 0;
-//        for (Integer el: arr ){
-//            if (arr.contains(n-el) ){
-//                int indexOfEl = arr.indexOf(el);
-//                int indexOfNMinusEl= arr.indexOf(n-el);
-//                arr.remove(indexOfNMinusEl);
-//                arr.remove(indexOfEl);
-//                arr.add(n+1);
-//                arr.add(n+1);
-//                counter++;
-//            }
-//        }
         for (int i = 0; i < arr.size(); i++){
             int el = arr.get(i);
-            int indexOfNMinusEl = arr.indexOf(n-el);
+            int indexOfNMinusEl = arr.lastIndexOf(n-el);
             if (arr.contains(n-el) && i != indexOfNMinusEl){
                   arr.set(indexOfNMinusEl, n+1);
                   arr.set(i, n+1);
                 counter++;
-                System.out.println(arr);
             }
         }
-
         return counter;
     }
-
 }
--- kata_wen_gui/pairs_java/pairs_java/src/test/java/PairsTest.java
+++ kata_wen_gui/pairs_java/pairs_java/src/test/java/PairsTest.java
@@ -12,6 +12,7 @@ public class PairsTest {
    private ArrayList<Integer> array2;
    private ArrayList<Integer> array3;
    private ArrayList<Integer> array4;
+   private ArrayList<Integer> array5;
 
 
     @Before
@@ -21,6 +22,7 @@ public class PairsTest {
         array2 = new ArrayList<>(Arrays.asList(2,4,8,8,8,12,14));
         array3 = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9,10));
         array4 = new ArrayList<>(Arrays.asList(4,4,4,4,4,4,4,4,4,4,4));
+        array5 = new ArrayList<>(Arrays.asList(-1, 0, 100));
     }
     @Test
     public void canFindPairsInArray1(){
@@ -44,5 +46,11 @@ public class PairsTest {
     public void canFind5PairsInArray4(){
         int actual = pairs.findPairs(array4, 8);
         assertEquals(5, actual);
+    }
+
+    @Test
+    public void canFind0PairsInArray5(){
+        int actual = pairs.findPairs(array5, 15);
+        assertEquals(0, actual);
     }
 }
