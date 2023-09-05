29da04cbf552b65f5c5326cad6af99df5398c78d
wenjingsheng <wenjing2018uk@gmail.com>
2023-09-05 15:00:52 +0100

    sorted by adding an extra condition

--- kata_wen_gui/pairs_java/pairs_java/src/main/java/Pairs.java
+++ kata_wen_gui/pairs_java/pairs_java/src/main/java/Pairs.java
@@ -20,12 +20,10 @@ public class Pairs {
 //        }
         for (int i = 0; i < arr.size(); i++){
             int el = arr.get(i);
-            if (arr.contains(n-el)){
-                int indexOfNMinusEl = arr.indexOf(n-el);
-                arr.remove(indexOfNMinusEl);
-                arr.remove(i);
-                arr.add(n+1);
-                arr.add(n+1);
+            int indexOfNMinusEl = arr.indexOf(n-el);
+            if (arr.contains(n-el) && i != indexOfNMinusEl){
+                  arr.set(indexOfNMinusEl, n+1);
+                  arr.set(i, n+1);
                 counter++;
                 System.out.println(arr);
             }
--- kata_wen_gui/pairs_java/pairs_java/src/test/java/PairsTest.java
+++ kata_wen_gui/pairs_java/pairs_java/src/test/java/PairsTest.java
@@ -20,7 +20,7 @@ public class PairsTest {
         array1 = new ArrayList<>(Arrays.asList(2,3,5,7,8));
         array2 = new ArrayList<>(Arrays.asList(2,4,8,8,8,12,14));
         array3 = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9,10));
-        array4 = new ArrayList<>(Arrays.asList(1,2,3,4));
+        array4 = new ArrayList<>(Arrays.asList(4,4,4,4,4,4,4,4,4,4,4));
     }
     @Test
     public void canFindPairsInArray1(){
@@ -37,6 +37,12 @@ public class PairsTest {
     @Test
     public void canFind5PairsInArray3(){
         int actual = pairs.findPairs(array3, 10);
+        assertEquals(5, actual);
+    }
+
+    @Test
+    public void canFind5PairsInArray4(){
+        int actual = pairs.findPairs(array4, 8);
         assertEquals(5, actual);
     }
 }
