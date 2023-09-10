79c9bc06752cb4087367ae3b1eb000d063d2ced3
GPSPic <picard.guillaume47@gmail.com>
2023-09-10 15:58:37 +0100

    getting clooooooser

--- kata_wen_gui/word_wrap_js/models/wordWrap.js
+++ kata_wen_gui/word_wrap_js/models/wordWrap.js
@@ -5,54 +5,40 @@ const wordWrap = function (str, num) {
   let endIndex = num;
 
       if (strLength <= num) {
-    arr.push(str);
-    return arr.length;
+    console.log(str);
+    return 1;
   }
 
   while (startIndex < strLength) {
     const subString = str.slice(startIndex, endIndex);
     const lastIndexOfSpace = subString.lastIndexOf(" ");
-      
-        console.log(startIndex);
-        if (lastIndexOfSpace !== -1) {
-              const subSubString = subString.slice(startIndex, lastIndexOfSpace);
-              startIndex = lastIndexOfSpace + 1;
+      if (lastIndexOfSpace !== -1) {
+              const subSubString = subString.slice(0, lastIndexOfSpace);
+              startIndex += lastIndexOfSpace + 1;
               endIndex = startIndex + num;
               arr.push(subSubString);
-        } else if (lastIndexOfSpace === -1) {
-                  arr.push(subString); 
-                  startIndex = endIndex
-                  endIndex = endIndex + num; 
-              
-             
-            
-              //     } else if (lastIndexOfSpace === -1 && subString.length > num) {
-              //           for (let i = 0; i < subString.length; i += num){
-              //                 subSubString = subString.slice(i, i + num)
-              //                 arr.push(subSubString);
-              //           } 
-              //           break;
-              
+      } else if (lastIndexOfSpace === -1) {
+            if ((startIndex + num) < strLength) {
+                  arr.push(subString +"-");
+            } else {
+                  arr.push(subString);                  
+            }
+            startIndex = endIndex;
+            endIndex = endIndex + num;
         }
  
   }
-  console.log(arr);
+
+  for (const el of arr) {
+      if (arr.indexOf(el) !== arr.length-1){
+            console.log(el.length);
+            console.log(el + "\n");
+      } else {
+            console.log(el);
+      }
+  }
+
   return arr.length;
 };
 
 module.exports = wordWrap;
-
-// const subString = str.slice(indexOfPreviousSpace, num)
-//       const lastIndexOfWhitespace = subString.lastIndexOf('\\s')
-//       if (lastIndexOfWhitespace > 0) {
-//             const subSubString = subString.slice(indexOfPreviousSpace, lastIndexOfWhitespace)
-//             indexOfPreviousSpace = lastIndexOfWhitespace + 1
-//             arr.push(subSubString)
-//       }
-// const stringLengthChecker = (str, num) => {
-//       if (remainingStringLength <= num) {
-//             console.log(str);
-//             arr.push(str)
-//             return 1
-//       }
-// }
--- kata_wen_gui/word_wrap_js/specs/wordWrap_specs.js
+++ kata_wen_gui/word_wrap_js/specs/wordWrap_specs.js
@@ -14,20 +14,26 @@ describe("wordWrap", function () {
             chemistry = "methylamphetalamine"
       })
 
-      it("returns 2 lines for an easy string with a column number of 8", function () {
-            const actual = wordWrap(easy, 8)
-            assert.strictEqual(actual,2)
+      // it("returns 2 lines for an easy string with a column number of 8", function () {
+      //       const actual = wordWrap(easy, 8)
+      //       assert.strictEqual(actual,2)
+      // })
+
+      // it("returns 1 lines for an easy string with a column number of 20", function () {
+      //       const actual = wordWrap(easy, 20)
+      //       assert.strictEqual(actual,1)
+      // })
+
+      // it("returns 1 lines for an easy string with a column number of 3", function () {
+      //       const actual = wordWrap(easy, 3)
+      //       assert.strictEqual(actual,4)
+      // })
+
+      // it('should return 1 line for the cat string with 50 length', function() {
+      //       assert.strictEqual(wordWrap(cat, 50), 1)
+      // })
+      
+      it('should return 2 lines for the cat string with 30 length', function() {
+            assert.strictEqual(wordWrap(cat, 30), 2)
       })
-
-      it("returns 1 lines for an easy string with a column number of 20", function () {
-            const actual = wordWrap(easy, 20)
-            assert.strictEqual(actual,1)
-      })
-
-      it("returns 1 lines for an easy string with a column number of 3", function () {
-            const actual = wordWrap(easy, 3)
-            assert.strictEqual(actual,4)
-      })
-
-
 })
